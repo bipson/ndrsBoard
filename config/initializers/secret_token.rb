91315adcbf3e81af16c23381393931bd18fd45ea
Philipp Raich <philipp.raich@gmail.com>
2013-11-12 21:37:56 +0100

    first steps (following rails tutorial)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NdrsBoard::Application.config.secret_key_base = 'df875c0208899236ff6716e41068968f0ef04e0a404287d0b5275ab00736ad13dab77c2d06730fecbc2b12a32a9e0bc7d866d208cabb3fcffaf5766f984608d7'
